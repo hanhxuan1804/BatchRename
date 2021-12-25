1d49e4b5d9613237ee3cea421f03ea403d706a47
hanhxuan1804 <hanhxuan1804@gmail.com>
2021-12-25 10:23:58 +0700

    Add project files.

---
+++ BatchRename/CStorageFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchRename
+{
+    public class CStorageFile : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string NewName { get; set; }
+        public string Path { get; set; }
+        public string Error { get; set; }
+        public CStorageFile(string name, string newname, string path, string err)
+        {
+            this.Name = name;
+            this.NewName = newname;
+            this.Path = path;
+            this.Error = err;
+        }
+
+        public CStorageFile()
+        {
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " " + this.NewName + " " + this.Path + " " + this.Error;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ BatchRename/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Fluent;
+
+namespace BatchRename
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        BindingList<CStorageFile> list = new BindingList<CStorageFile>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RibbonWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            listFiles.ItemsSource = list;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.ShowDialog();
+           
+            foreach (string filename in openFileDialog.FileNames)
+            {
+                CStorageFile f = new CStorageFile();
+                f.Name = filename;
+               
+              // f.Name = Path.GetFileName(filename);
+                list.Add(f);
+            }
+           
+        }
+    }
+}
